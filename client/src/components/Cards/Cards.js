26cda212ca38b305de3a9f2eead6496ee98496d1
arifzamil <azamil@macalester.edu>
2018-10-10 10:53:18 -0500

    added flop to cards

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -6,37 +6,65 @@ class Cards extends Component {
     constructor(props){
         super(props);
         this.state= {
-            userHand: this.props.userHand
+            userHand: this.props.userHand,
+            flop: this.props.flop
         };
     }
 
 
     render (){
         return (
-            <div style={{display:"flex",
-                margin:"auto",
-                borderStyle:"dashed",
-                borderColor:"blue",
-                backgroundColor:"grey",
-                height:150,
-                width:200,
-                position:"absolute",
-                bottom: 10,
-                right: "43%"
-            }}>
-                {
-                    this.state.userHand.map((card)=>(
-                        <div style={{display:"flex",
-                            margin:"auto",
-                            borderStyle:"solid",
-                            borderColor:"blue",
-                            backgroundColor:"white",
-                            height:120,
-                            width:80}}>
-                            <p style={{margin:"auto", textAlign:"center"}}>{card}</p>
-                        </div>
-                    ))
-                }
+            <div>
+                <div style={{display:"flex",
+                    margin:"auto",
+                    borderStyle:"dashed",
+                    borderColor:"blue",
+                    backgroundColor:"grey",
+                    height:150,
+                    width:200,
+                    position:"absolute",
+                    bottom: 10,
+                    right: "43%"
+                }}>
+                    {
+                        this.state.userHand.map((card)=>(
+                            <div style={{display:"flex",
+                                margin:"auto",
+                                borderStyle:"solid",
+                                borderColor:"blue",
+                                backgroundColor:"white",
+                                height:120,
+                                width:80}}>
+                                <p style={{margin:"auto", textAlign:"center"}}>{card}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div style={{display:"flex",
+                    margin:"auto",
+                    borderStyle:"dashed",
+                    borderColor:"blue",
+                    backgroundColor:"grey",
+                    height:150,
+                    width:300,
+                    position:"absolute",
+                    bottom: "40%",
+                    right: "40%"
+                }}>
+                    {
+                        this.state.flop.map((card)=>(
+                            <div style={{display:"flex",
+                                margin:"auto",
+                                borderStyle:"solid",
+                                borderColor:"blue",
+                                backgroundColor:"white",
+                                height:120,
+                                width:80}}>
+                                <p style={{margin:"auto", textAlign:"center"}}>{card}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         )
     }
