2c6d281e9bb5f61473b008654fb9c56703d11cbd
arifzamil <azamil@macalester.edu>
2018-10-10 09:30:08 -0500

    updating cards

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -3,10 +3,22 @@ import React, { Component } from "react";
 //The Cards class is responsible for rendering the community cards and the hole cards.
 class Cards extends Component {
 
-    render () {
+    constructor(props){
+        super(props);
+        this.state= {
+            userHand: this.props.userHand
+        };
+    }
+
+
+    render (){
         return (
             <div>
-
+                {
+                    this.state.userHand.map((card)=>(
+                        <div style={}> {card} </div>
+                    ))
+                }
             </div>
         )
     }
