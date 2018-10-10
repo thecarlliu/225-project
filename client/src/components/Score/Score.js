d0c86b17c87f8bfb13468c113447b07ff8ecd2e1
michelleas <marmstr2@macalester.edu>
2018-10-10 14:11:59 -0500

    Score shows up on screen

--- client/src/components/Score/Score.js
+++ client/src/components/Score/Score.js
@@ -1,11 +1,26 @@
 import React, {Component} from "react";
 
 class Score extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            curScore: this.props.curScore
+        }
+    }
     render() {
         return(
-            <div></div>
+            <div style = {{fontFamily: "Georgia"}}>
+                <h1>
+                    Score: {this.state.curScore}
+                </h1>
+            </div>
         )
     }
+}
+
+function updateScore(){
+    curScore += 10; // arbitrary
 }
 
 export default Score
