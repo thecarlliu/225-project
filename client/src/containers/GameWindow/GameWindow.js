eca6f12cf1bfc55baf4f54cce2956f7197cd8ebb
arifzamil <azamil@macalester.edu>
2018-10-10 15:42:41 -0500

    updates to cards and game window

--- client/src/containers/GameWindow/GameWindow.js
+++ client/src/containers/GameWindow/GameWindow.js
@@ -9,10 +9,12 @@ import NavBar from "../../components/NavBar";
 import PlayButton from "../../components/PlayButton";
 
 //The GameWindow keeps track of the state of the game.
+
+
 const deck = ["AH","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
-              "AS","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS",
-              "AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
-              "AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC"];
+    "AS","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS",
+    "AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
+    "AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC"];
 
 class GameWindow extends Component {
 
@@ -26,6 +28,8 @@ class GameWindow extends Component {
     //Deal the hole cards
     //Handle Input
     //Update HighScore
+
+
 
     constructor(props){
         super(props);
@@ -41,7 +45,8 @@ class GameWindow extends Component {
                 <NavBar />
                 <PlayButton />
                 <Timer />
-                <Cards userHand={[this.state.hand[0], this.state.hand[1]]} flop={[this.state.hand[2], this.state.hand[3], this.state.hand[4]]}/>
+                <Cards userHand={[this.state.hand[0], this.state.hand[1]]}
+                       flop={[this.state.hand[2], this.state.hand[3], this.state.hand[4]]}/>
                 <Score />
                 <Input />
                 <HighScore />
