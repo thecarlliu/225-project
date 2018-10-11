31ac23bfbbd70b1a1038fa4f39fc27868feb72eb
michelleas <marmstr2@macalester.edu>
2018-10-10 23:52:38 -0500

    Added updateScore and resetScore methods

--- client/src/components/Score/Score.js
+++ client/src/components/Score/Score.js
@@ -1,26 +1,39 @@
 import React, {Component} from "react";
 
+/**
+ * Renders the current score
+ */
 class Score extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            curScore: this.props.curScore
+            curScore: this.props.curScore,
+            incr: this.props.incr
         }
     }
+
+    updateScore(){
+        let newState = this.state;
+        newState.curScore += newState.incr;
+        this.setState(newState);
+    }
+
+    resetScore(){
+        let newState = this.state;
+        newState.curScore = 0;
+        this.setState(newState);
+    }
+
     render() {
         return(
-            <div style = {{fontFamily: "Georgia"}}>
-                <h1>
+            <div style = {{fontFamily: "Georgia", position: "fixed", bottom: 550, left: 150}}>
+                <h3>
                     Score: {this.state.curScore}
-                </h1>
+                </h3>
             </div>
         )
     }
-}
-
-function updateScore(){
-    curScore += 10; // arbitrary
 }
 
 export default Score
