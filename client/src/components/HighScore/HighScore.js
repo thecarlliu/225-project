71c4066a7e22aa50ddc0e7b5a5097fa07444c96f
michelleas <marmstr2@macalester.edu>
2018-10-10 19:52:06 -0500

    Added high score component

--- client/src/components/HighScore/HighScore.js
+++ client/src/components/HighScore/HighScore.js
@@ -1,9 +1,17 @@
 import React, {Component} from "react";
 
 class HighScore extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            highScore: this.props.highScore
+        }
+    }
     render() {
         return (
-            <div></div>
+            <div>
+                <div style = {{fontFamily: "Georgia", }}></div>
+            </div>
         )
     }
 }
