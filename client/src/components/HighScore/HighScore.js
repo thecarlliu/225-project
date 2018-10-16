1c2b69540e44f74229bf9d5963e6bb1eeecfaba2
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-10-16 10:47:27 -0500

    Fixed score upddates.

--- client/src/components/HighScore/HighScore.js
+++ client/src/components/HighScore/HighScore.js
@@ -6,16 +6,13 @@ import React, {Component} from "react";
 class HighScore extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            highScore: this.props.highScore
-        }
     }
     render() { // sets the position of the high score text
         return (
             <div>
                 <div style = {{fontFamily: "Georgia", position: "fixed", bottom: 550, right: 150}}>
                     <h3>
-                        High Score: {this.state.highScore}
+                        High Score: {this.props.highScore}
                     </h3>
                 </div>
             </div>
