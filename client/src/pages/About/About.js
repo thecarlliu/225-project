40b578eb428fb28cb461bc87f45bf7c82d02da2c
arifzamil <azamil@macalester.edu>
2018-10-29 18:28:48 -0500

    edits to rules and about

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,23 +1,26 @@
 import React, {Component} from "react";
 import NavButton from "../../components/NavButton/NavButton";
 
+const placement =  {margin: "auto", textAlign:"left", width:"75%"};
+const liPlacement = {margin: "auto", textAlign:"left", width:"70%"};
+
 class About extends Component {
     render () {
         return (
             <div>
-                <NavButton label = {"Home"} route = {"/home"}/>
-                <NavButton label = {"Rules"} route = {"/rules"}/>
-                <h1>About</h1>
-                <p>Welcome to Texas Goat'em!</p>
-                    <p>In this fun game, become a better poker player by learning how to count cards.</p>
-                    <div>Development team:</div>
-                        <li>Michelle Armstrong-Spielberg</li>
-                        <li>George Clare Kennedy</li>
-                        <li>Carl Liu</li>
-                        <li>Arif Zamil</li>
-                    <p>Special thanks to Shilad Sen and all of the other students in our section for all of your help
+                <NavButton left={"5%"} label = {"Home"} route = {"/home"}/>
+                <NavButton left={"95%"} label = {"Rules"} route = {"/rules"}/>
+                <h1 style={{margin: "auto", textAlign:"center"}}>About</h1>
+                <p style={{margin: "auto", textAlign:"center"}}>Welcome to Texas Goat'em!</p>
+                    <p style={placement}>In this fun game, become a better poker player by learning how to count cards.</p>
+                    <div style={placement}>Development team:</div>
+                        <li style={liPlacement}>Michelle Armstrong-Spielberg</li>
+                        <li style={liPlacement}>George Clare Kennedy</li>
+                        <li style={liPlacement}>Carl Liu</li>
+                        <li style={liPlacement}>Arif Zamil</li>
+                    <p style={placement}>Special thanks to Shilad Sen and all of the other students in our section for all of your help
                         and input.</p>
-                <footer>(c) 2018 GOATS. Macalester College.</footer>
+                <footer style={{margin:"auto", position:"absolute", bottom:10}}>(c) 2018 GOATS. Macalester College.</footer>
             </div>
         )
     }
--- client/src/pages/Rules/Rules.js
+++ client/src/pages/Rules/Rules.js
@@ -5,10 +5,10 @@ class Rules extends Component {
     render() {
         return (
             <div>
-                <NavButton label = {"Home"} route = {"/home"}/>
-                <NavButton label = {"About"} route = {"/about"}/>
-                <h3>Rules</h3>
-                <p>You are dealt two cards and shown the flop. Based on the cards in your hand, count the number of outs
+                <NavButton left={"5%"} label = {"Home"} route = {"/home"} style={{display:"inline-block"}}/>
+                <NavButton left={"95%"} label = {"About"} route = {"/about"} style={{display:"inline-block"}}/>
+                <h1 style={{margin: "auto", textAlign:"center"}}>Rules</h1>
+                <p style={{margin: "auto", textAlign:"left", width:"75%"}}> You are dealt two cards and shown the flop. Based on the cards in your hand, count the number of outs
                     you have. Enter the number of outs into the input box. If you answered correctly, your score will
                     increase and you will be dealt another hand and shown a different flop. If not, the game ends. The
                     more you answer correctly, the higher your score will get. But be careful -- as you keep answering
