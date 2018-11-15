f9443c4fe597078462106b4366035527f83ea37f
CarL Liu <thecarlliu99@CarLs-Air.domain>
2018-11-14 22:20:00 -0600

    removed unnecessary styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Routes from "./Routes";
 class App extends Component {
     render() {
         return (
-            <div className={"app-container"} style={{backgroundColor:"#98FFBA", height:"700px", width:"100%"}}>
+            <div className={"app-container"}>
                 <Routes />
             </div>
         )
