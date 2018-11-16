1a7febef0cf173c7eb894634eaafbb0bc6a9bae5
arifzamil <azamil@macalester.edu>
2018-11-16 12:21:31 -0600

    tweaks to rules

--- client/src/pages/Rules/Rules.js
+++ client/src/pages/Rules/Rules.js
@@ -2,26 +2,24 @@ import React, {Component} from "react";
 import NavBar from "../../components/NavBar";
 
 const header = {margin: "auto", textAlign:"center"};
-const para = {margin: "auto", textAlign:"left", width:"75%"}
-const divvy = {padding: "10px", display: "block", position:"fixed"}
+const para = {margin: "auto", textAlign:"left", width:"75%"};
+const divvy = {padding: "10px", display: "block", position:"absolute"};
 
 
 class Rules extends Component {
     render() {
         return (
-            <div>
-                <NavBar />
-                <div style={{position: "fixed", top: "50px", color: "white", overflow:"hidden"}}>
-                    <div style={{padding:"10px"}}>
-                        <h1 style={header}>Rules</h1>
+            <div style={{overflow:"scroll"}}>
+                <div style={{position: "absolute", top: "50px", color: "white", display:"block"}}>
+                    <div style={{padding:"15px"}}>
+                        <h1 style={header}>RULES</h1>
                         <p style={para}> You are dealt two cards and shown the flop. Based on the cards in your hand, count the number of outs
                             you have. Enter the number of outs into the input box. If you answered correctly, your score will
                             increase and you will be dealt another hand and shown a different flop. If not, the game ends. The
                             more you answer correctly, the higher your score will get. But be careful -- as you keep answering
                             correctly, you will also have less time to answer.</p>
                     </div>
-
-                    <div>
+                    <div style={{padding:"15px"}}>
                         <h2 style={header}>What is an Out?</h2>
                         <br/>
                         <p style={para}>
@@ -35,17 +33,18 @@ class Rules extends Component {
 
                     <br/><br/>
 
-                    <div  style={{position: "fixed", top: "250px", width:"100%"}}>
+                    <div  style={{position: "absolute", top: "250px", width:"100%", display:"block"}}>
                         <h2 style={header}>Types of Outs:</h2>
+                        <br/>
                         <p style={para}>
                             <i><b>Flush Draw:</b></i> A flush draw
                             is a hand with four cards of the same suit that may improve
                             to a flush. There are 13 cards of a given suit in a deck of cards,
                             but to have a draw you are using 4 of the same suit, therefore you have
-                            9 outs.
+                            9 outs. See example below for a flush draw:
                         </p>
 
-                        <div style={{position: "fixed", left: "50%", padding:"20px", marginLeft:"-200px"}}>
+                        <div style={{position: "absolute", left: "50%", padding:"20px", marginLeft:"-230px"}}>
                             <img src={"images/AH.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
                             <img src={"images/10H.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
                             <img src={"images/3C.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
@@ -54,18 +53,16 @@ class Rules extends Component {
                         </div>
                     </div>
 
-                    <br/><br/>
-
-                    <div style={{position: "fixed", top: "550px", width:"100%"}}>
+                    <div style={{position: "absolute", top: "550px", width:"100%", display:"block"}}>
                         <p style={para}>
                             <i><b>Inside Straight Draw:</b></i> An inside straight draw inspect
                             a hand with four of the five cards needed for a straight,
                             but missing one in the middle. You are missing one possible card to give
                             you a straight, and this card appears in the deck four times, (one per suit)
-                            so you have four outs
+                            so you have four outs. See example below for an inside straight draw:
                         </p>
 
-                        <div style={{position: "fixed", left: "50%", padding:"20px", marginLeft:"-200px"}}>
+                        <div style={{position: "absolute", left: "50%", padding:"20px", marginLeft:"-230px"}}>
                             <img src={"images/3H.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
                             <img src={"images/4S.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
                             <img src={"images/5C.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
@@ -74,19 +71,17 @@ class Rules extends Component {
                         </div>
                     </div>
 
-                    <br/><br/>
-
-                    <div style={{position: "fixed", top: "850px", width: "100%"}}>
+                    <div style={{position: "absolute", top: "800px", width: "100%", display:"block"}}>
                         <p style={para}>
                             <i><b>Outside Straight Draw:</b></i> An outside straight draw
                             is a hand with four of the five needed cards in sequence
                             (and could be completed on either end) that may improve to a straight.
                             Because you have two different numbers that could complete a straight,
                             and because there are four of each number in a deck, this gives you
-                            eight outs.
+                            eight outs. See example below for an outside straight draw:
                         </p>
 
-                        <div style={{position: "fixed", left: "50%", padding:"20px", marginLeft:"-200px"}}>
+                        <div style={{position: "absolute", left: "50%", padding:"20px", marginLeft:"-230px"}}>
                             <img src={"images/5H.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
                             <img src={"images/6C.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
                             <img src={"images/7C.png"} style={{width: "80px", height: "120px", padding:"5px"}}/>
@@ -95,6 +90,8 @@ class Rules extends Component {
                         </div>
                     </div>
                 </div>
+
+                    <NavBar />
             </div>
         )
     }
