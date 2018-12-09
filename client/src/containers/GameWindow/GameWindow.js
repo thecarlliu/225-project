c69a06a5016864f36519fade34ab4f0676a533cd
arifzamil <azamil@macalester.edu>
2018-12-09 11:32:32 -0600

    popup styles

--- client/src/containers/GameWindow/GameWindow.js
+++ client/src/containers/GameWindow/GameWindow.js
@@ -272,14 +272,8 @@ class GameWindow extends Component {
                       display: this.state.popUpCardsShowing
                     }}>
                         <img src={"images/"+this.state.userHand[0]+".png"} style={{width: "40px", height: "60px", padding:"5px"}}/>
-<<<<<<< HEAD
                         <img src={"images/"+this.state.userHand[1]+".png"} style={{width: "40px", height: "60px", padding:"5px", paddingRight:"10px"}}/>
                         <img src={"images/"+this.state.flop[0]+".png"} style={{width: "40px", height: "60px", padding:"5px", paddingLeft:"10px"}}/>
-=======
-                        <img src={"images/"+this.state.userHand[1]+".png"} style={{width: "40px", height: "60px", padding:"5px"}}/>
-                        <div class="vl"></div>
-                        <img src={"images/"+this.state.flop[0]+".png"} style={{width: "40px", height: "60px", padding:"5px"}}/>
->>>>>>> 5153899ed31e9177b5ee4650b08941e8be58f13d
                         <img src={"images/"+this.state.flop[1]+".png"} style={{width: "40px", height: "60px", padding:"5px"}}/>
                         <img src={"images/"+this.state.flop[2]+".png"} style={{width: "40px", height: "60px", padding:"5px"}}/>
                     </div>
